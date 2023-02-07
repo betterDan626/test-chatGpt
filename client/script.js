5fd2d5aebaaaaea14367f41e3c37a926b61a009d
betterDan626 <cathyDai626@gmail.com>
2023-02-07 23:26:25 +0800

    commit

--- client/script.js
+++ client/script.js
@@ -89,7 +89,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    // const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://test-chat-p4fa.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
